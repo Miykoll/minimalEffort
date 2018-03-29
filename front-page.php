ce085e1158b09cacca598fc60aea6b2c3ad1ad8c
Michael ten Den <info@michaeltenden.nl>
2018-03-29 20:46:35 +0200

    Frontpage image

--- front-page.php
+++ front-page.php
@@ -4,6 +4,7 @@
     <div class="small-container">
         <h1>Michael ten Den</h1>
         <h5>#Programmer, #Engineer, #LabVIEWer, #Scubadiver, #BBoy, #Traveler</h5>
+        <img src="<?php echo get_bloginfo('template_directory'); ?>/images/terminal.png" alt="Terminal image">
     </div>
 </header>
 
