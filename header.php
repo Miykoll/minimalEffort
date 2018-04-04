e9633062d554be5450b0aa22f7b062e83a2d59ad
Michael ten Den <info@michaeltenden.nl>
2018-04-04 16:52:51 +0200

    Added google analytics

--- header.php
+++ header.php
@@ -9,23 +9,17 @@
   <meta name="author" content="">
 
  
-  <!-- Google Analytics 
-  <script>
-    (function(i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r;
-      i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date();
-      a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-    ga('create', 'UA-42068444-1', 'auto');
-    ga('send', 'pageview');
-  </script>
-  -->
+  <!-- Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-96599848-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-96599848-1');
+</script>
+
+  
   <!-- Google Adwords 
   <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
   </script>
