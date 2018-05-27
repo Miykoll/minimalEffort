21b8dbfbaa16a172bec2035c2ece842478cfd27b
Michael ten Den <info@michaeltenden.nl>
2018-05-27 12:05:43 +0200

    Added container with content-single to page.php, no idea why I did that

--- page.php
+++ page.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 
+    <div class="container">
 
+        <?php 
+            if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+                get_template_part( 'content-single', get_post_format() );
+                
+
+            endwhile; endif; 
+        ?>
+
+    </div>
 
 <?php get_footer(); ?>
